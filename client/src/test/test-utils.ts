e616ba096ca3fa5808eea88f469b7a153e50a763
vcb88 <xtile@yandex.com>
2025-01-17 23:45:29 +0000

    chore: remove test files and configurations - Remove all test directories - Remove test configuration files - Remove test scripts from package.json - Keep testing documentation

--- client/src/test/test-utils.ts
+++
@@ -1,9 +0,0 @@
-import { Socket } from 'socket.io-client';
-
-export type MockSocket = Socket & {
-    mockEmit: jest.Mock;
-    mockOn: jest.Mock;
-    mockOff: jest.Mock;
-    mockClose: jest.Mock;
-    simulateEvent: (event: string, data: unknown) => Promise<void>;
-};
