7aee5cc33dde83b8c9baf4cd60d04d20b2cec494
vcb88 <xtile@yandex.com>
2025-01-17 20:41:25 +0000

    fix(shared): fix type errors in events utils - Add required gameId parameter to createGameEvent - Fix validation of playerNumber field in validateEvent - Improve function documentation

--- shared/src/utils/events.ts
+++ shared/src/utils/events.ts
@@ -2,10 +2,14 @@ import type { GameEvent, WebSocketEvent } from '../types/base/types.js';
 
 /**
  * Create a game event
+ * @param type Type of the event
+ * @param gameId ID of the game this event belongs to
+ * @param data Optional event data
  */
-export const createGameEvent = <T>(type: WebSocketEvent, data?: T): GameEvent => ({
+export const createGameEvent = <T>(type: WebSocketEvent, gameId: string, data?: T): GameEvent => ({
     type,
     id: crypto.randomUUID(),
+    gameId,
     timestamp: Date.now(),
     data: data || undefined
 });
@@ -29,7 +33,7 @@ export const validateEvent = (event: unknown): event is GameEvent => {
         typeof e.type === 'string' &&
         typeof e.id === 'string' &&
         typeof e.timestamp === 'number' &&
-        (e.gameId === undefined || typeof e.gameId === 'string') &&
-        (e.player === undefined || typeof e.player === 'number')
+        typeof e.gameId === 'string' &&
+        (e.playerNumber === undefined || typeof e.playerNumber === 'number')
     );
 };
